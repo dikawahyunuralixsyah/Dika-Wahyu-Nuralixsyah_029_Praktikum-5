e3f78682c305afe078b4931d87f8660b88cb223f
dikawahyunuralixsyah <90248183+dikawahyunuralixsyah@users.noreply.github.com>
2021-11-14 20:40:33 +0700

    Add files via upload

---
+++ Dika Wahyu Nuralixsyah_029_Program 24.cpp
@@ -0,0 +1,18 @@
+/* Program sederhana menggunakan fungsi */
+/* yang tidak mengembalikan nilai */ 
+
+#include<stdio.h>
+#include<conio.h>
+
+void garis();
+main()							/*deklarasi fungsi */
+{
+	garis();
+	printf("NO       NIM         NAMA         NILAI  \n");
+	garis(); 
+	getch();
+}
+void garis() 					/* definisi fungsi */
+{
+	printf("==================================== \n");
+	}
